69cca160ca2df2f9f1fdf629df79b3f1201b1bc6
codecly259 <maxinchun0215@qq.com>
2016-05-06 15:12:32 +0800

    使用阻塞案例说明之前的不合理

--- requestHandlers.js
+++ requestHandlers.js
@@ -2,8 +2,20 @@
  * Created by maxinchun on 2016/5/6.
  */
 
+/**
+ * node中程序是单线程执行的，通过事件轮询来实现并行操作
+ * @returns {string}
+ */
 function start() {
 	console.log("Request handler 'start' was called.");
+
+	function sleep(milliSeconds) {
+		var startTime = new Date().getTime();
+		console.info(new Date().getTime() < startTime + milliSeconds);
+		while(new Date().getTime() < startTime + milliSeconds);
+	}
+
+	sleep(10000);
 	return "Hello Start";
 }
 
