0d716b54c7006083d5916b40caa34fef4f8ff860
codecly259 <maxinchun0215@qq.com>
2016-05-06 14:35:03 +0800

    使用router模块来处理请求

--- index.js
+++ index.js
@@ -3,5 +3,7 @@
  */
 
 var server = require("./server");
+var router = require("./router");
 
-server.start();
+server.start(router.route);
+
---
+++ router.js
@@ -0,0 +1,9 @@
+/**
+ * Created by maxinchun on 2016/5/6.
+ */
+
+function route(pathname) {
+	console.log("About to route a request for " + pathname);
+}
+
+exports.route = route;
