ec432b7a78f5250bc564832f0e211bbc1ed14794
codecly259 <maxinchun0215@qq.com>
2016-05-06 14:28:07 +0800

    重构，分离server为模块 并使用

---
+++ index.js
@@ -0,0 +1,7 @@
+/**
+ * Created by maxinchun on 2016/5/6.
+ */
+
+var server = require("./server");
+
+server.start();
--- server.js
+++ server.js
@@ -1,16 +1,24 @@
 /**
  * Created by maxinchun on 2016/5/6.
+ *
+ * 理解nodejs:Understanding node.js  http://debuggable.com/posts/understanding-node-js:4bd98440-45e4-4a9a-8ef7-0f7ecbdd56cb
+ *
+ * @type {exports|module.exports}
  */
 
 var http = require("http");
 
-function onRequest(request, response) {
-	console.log("Request received.");
-	response.writeHead(200, {"Content-Type": "text/plain"});
-	response.write("Hello World.");
-	response.end();
+function start() {
+	function onRequest(request, response) {
+		console.log("Request received.");
+		response.writeHead(200, {"Content-Type": "text/plain"});
+		response.write("Hello World.");
+		response.end();
+	}
+
+	http.createServer(onRequest).listen(8888);
+	console.log("Server has started.");
 }
 
-http.createServer(onRequest).listen(8888);
+exports.start = start;
 
-console.log("Server has started.");
