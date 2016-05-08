7f75b185bd08db280b98dab96e0b38245dbad690
codecly259 <maxinchun0215@qq.com>
2016-05-09 00:01:36 +0800

    新增课程二

---
+++ node-lessons/lesson02/README.md
@@ -0,0 +1,19 @@
+# 学习使用外部模块
+
+## 目标
+
+在浏览器访问 `http://127.0.0.1:3000?q=codecly`时，输出`codecly`的md5值 `743109ac7ec36b08316240906e716056`
+
+## 步骤
+
+1. 创建目录： `mkdir lesson02 && cd lesson02`
+
+2. 初始化node项目环境： `npm init` ，根据要求填写配置，不需要中医具体配置，可以使用默认的
+
+3. 安装需要的依赖： `npm install express utility --save`
+
+4. 创建app.js并编写代码
+
+5. 运行项目： node app.js
+
+6. 访问网址[http://127.0.0.1:3000?q=codecly](http://127.0.0.1:3000?q=codecly)
---
+++ node-lessons/lesson02/app.js
@@ -0,0 +1,19 @@
+// 引入依赖
+var express = require("express");
+var utility = require("utility");
+
+// 建立express实例
+var app = express();
+
+app.get("/", function (request, response) {
+	// 获取参数值
+	var q = request.query.q;
+	// 使用工具得到md5之后的值
+	var md5Value = utility.md5(q);
+
+	response.send(q + ":" + md5Value);
+});
+
+app.listen("3000", function (request, response) {
+	console.log("app is running at port 3000");
+});
